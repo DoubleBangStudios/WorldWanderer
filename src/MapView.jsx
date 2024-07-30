7be48e62ad6f31c61f3fb3b712309982b18fdbb7
m-grainger <grainger.pdx@gmail.com>
2024-07-30 15:30:34 -0700

    feat: updated map component to utilize markers

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -1,8 +1,16 @@
 import "./MapView.css";
-import React from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useState } from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 const MapView = () => {
+  const [coords, setCoords] = useState({
+    lat: null,
+    lng: null,
+  });
+
+  const allValuesNull = (obj) =>
+    Object.values(obj).every((value) => value === null);
+
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS,
@@ -17,13 +25,25 @@ const MapView = () => {
     zIndex: "1000",
   };
 
+  const handleClickedMap = (e) => {
+    const newCoords = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+    setCoords(newCoords);
+  };
+
   return isLoaded ? (
     <div className="container-view">
       <GoogleMap
         mapContainerStyle={mapViewStyle}
         zoom={1}
-        center={{ lat: 0, lng: 0 }}
-      />
+        center={coords}
+        onClick={handleClickedMap}
+      >
+        <Marker
+          position={
+            !allValuesNull(coords) ? { lat: coords.lat, lng: coords.lng } : {}
+          }
+        />
+      </GoogleMap>
     </div>
   ) : (
     <div></div>
--- src/StreetView.jsx
+++ src/StreetView.jsx
@@ -1,5 +1,5 @@
 import "./StreetView.css";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
   GoogleMap,
   useJsApiLoader,
